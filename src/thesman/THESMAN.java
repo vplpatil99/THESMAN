572a6c583768820ff92410045dbb1bd777a51e24
vipul patil <41115168+vplpatil99@users.noreply.github.com>
2018-07-12 14:19:01 +0530

    VIPUL COMMIT

--- src/thesman/THESMAN.java
+++ src/thesman/THESMAN.java
@@ -15,7 +15,10 @@ public class THESMAN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("yasd");
+       
+       
+       System.out.println("he");
     }
     
 }
