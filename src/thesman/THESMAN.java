f80af1750f91b81db303ebb295ab7eb1bae83c40
vipul patil <41115168+vplpatil99@users.noreply.github.com>
2018-07-13 16:10:53 +0530

    VIPUL COMMIT

--- src/thesman/THESMAN.java
+++ src/thesman/THESMAN.java
@@ -18,10 +18,11 @@ public class THESMAN {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ResponseException, NotFound {
-        M160by2 m160by2=new M160by2();
+        new Login();
+       /* M160by2 m160by2=new M160by2();
         m160by2.login("9821958067","64145282");
         m160by2.sendSMS("Welcome THESMAN","9821958067"); 
-        m160by2.sendEmail("vipul.patil@primelenses.com","vplpatil99","vplpatil99@gmail.com","Welcome THESMAN","Welcome THESMAN!");  
+        m160by2.sendEmail("vipul.patil@primelenses.com","vplpatil99","vplpatil99@gmail.com","Welcome THESMAN","Welcome THESMAN!");  */
     }
     
 }
